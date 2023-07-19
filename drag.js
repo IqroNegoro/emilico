6465bb5d5c578efe26f11baee9ba999b9a65edaf
IqroNegoro <iqronegoro0@gmail.com>
2023-07-19 15:11:12 +0700

    add moving elemen

---
+++ drag.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const draggableElement = document.querySelectorAll(".floating");
+    let target;
+    let pos = {
+    elX: 0,
+    elY: 0,
+    x: 0,
+    y: 0
+}
+
+
+const handleMoveMouse = ({clientX: x, clientY: y}) => {
+    let newX = x - pos.x;
+    let newY = y - pos.y;
+    target.style.left = `${pos.elX + newX}px`;
+    target.style.top = `${pos.elY + newY}px`;
+}
+
+const handleMouseUp = () => {
+    document.removeEventListener("mousemove", handleMoveMouse);
+    document.removeEventListener("mouseup", handleMouseUp);
+}
+
+draggableElement.forEach(v => {
+    v.addEventListener("mousedown", ({clientX: x, clientY: y, currentTarget}) => {
+        target = v;
+        let {x: elX, y: elY} = currentTarget.getBoundingClientRect();
+        pos = {
+            elX,
+            elY,
+            x,
+            y
+        }
+
+        document.addEventListener("mousemove", handleMoveMouse);
+        document.addEventListener("mouseup", handleMouseUp);
+    })
+})
+})
--- gsap.js
+++ gsap.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-    gsap.fromTo(".animate-up", {
-        y: 10,
-        opacity: 0,
-        duration: 1,
-    }, {
-        y: 0,
-        opacity: 1,
-        stagger: 0.25,
-        onComplete: () => {
-            gsap.fromTo(".animate-right", {
-                x: -10,
-                opacity: 0,
-                duration: 1
-            }, {
-                x: 0,
-                opacity: 1,
-                stagger: 0.25
-            })
-        }
-    })
-})
+// document.addEventListener("DOMContentLoaded", () => {
+//     gsap.fromTo(".animate-up", {
+//         y: 10,
+//         opacity: 0,
+//         duration: 1,
+//     }, {
+//         y: 0,
+//         opacity: 1,
+//         stagger: 0.25,
+//         onComplete: () => {
+//             gsap.fromTo(".animate-right", {
+//                 x: -10,
+//                 opacity: 0,
+//                 duration: 1
+//             }, {
+//                 x: 0,
+//                 opacity: 1,
+//                 stagger: 0.25
+//             })
+//         }
+//     })
+// })
