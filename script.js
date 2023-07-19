0040d95e34ce8636919c157036db18047e0edb8e
IqroNegoro <iqronegoro0@gmail.com>
2023-07-19 11:52:24 +0700

    pushing:

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let fetchingWeather = async () => {
-    let {data} = await axios.get('http://dataservice.accuweather.com/currentconditions/v1/202513?apikey=TADziGdDmLttfBFX2HAGTZGcSCKNJUvx');
+    let {data} = await axios.get('https://dataservice.accuweather.com/currentconditions/v1/202513?apikey=TADziGdDmLttfBFX2HAGTZGcSCKNJUvx');
     document.getElementById("weather").innerHTML = `${data[0].Temperature.Metric.Value}&deg;`
 }
 
