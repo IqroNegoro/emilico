3d8d7f75e35bc368c06baf159e9437f0c79c3757
IqroNegoro <iqronegoro0@gmail.com>
2023-07-19 13:55:09 +0700

    Create timezone

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let fetchingWeather = async () => {
-    let {data} = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=-6.7063&longitude=108.557&current_weather=true');
-    console.log(data)
-    document.getElementById("weather").innerHTML = `${data.current_weather.temperature}&deg;`
+    let {current_weather} = await fetch('https://api.open-meteo.com/v1/forecast?latitude=-6.7063&longitude=108.557&current_weather=true').then(res => res.json());
+    document.getElementById("weather").innerHTML = `${current_weather.temperature}&deg;`
 }
 
 fetchingWeather();
+
+let date = new Intl.DateTimeFormat("id", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: 'Asia/Jakarta'
+}).format(+new Date())
+
+document.getElementById("date").textContent = date;
