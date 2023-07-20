25ef77cc31b41e2ba6072915a9335adf94537259
IqroNegoro <iqronegoro0@gmail.com>
2023-07-20 13:34:21 +0700

    Create floating effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'floating': 'floating 5s ease-in-out infinite'
+      },
+      keyframes: {
+        'floating': {
+          '0%': {trasform: 'translateY(0)'},
+          '50%': {transform: 'translateY(-20px)'},
+          '100%': {trasform: 'translateY(0)'}
+        }
+      }
+    },
   },
   plugins: [],
 }
